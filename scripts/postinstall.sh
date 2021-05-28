ccada06f46cd5d3039dd746526c9f47283e5ec08
Thong Le <thong.le@ihealthlabs.com>
2021-05-28 13:31:01 -0700

    Add iHealth device manager package. Note: might require npm login

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,11 @@
-#!/bin/zsh
+# #!/bin/zsh
+
+rm -rf ~/Library/Caches/CocoaPods
+echo y | rm -rf ~/Library/Developer/Xcode/DerivedData/*
+
+cd ios
+rm -rf Pods
+rm Podfile.lock
 
 arch_name="$(uname -m)"
  
@@ -8,14 +15,14 @@ if [ "${arch_name}" = "x86_64" ]; then
   else
       echo "Running on native Intel"
   fi 
-  cd ios
   pod install
 elif [ "${arch_name}" = "arm64" ]; then
   echo "Running on M1"
   sudo arch -x86_64 gem install ffi
   # https://github.com/CocoaPods/CocoaPods/issues/10518#issuecomment-798912624
-  cd ios
   arch -x86_64 pod install
 else
   echo "Unknown architecture: ${arch_name}"
 fi
+
+npm install --save @ihealth/ihealthlibrary-react-native
